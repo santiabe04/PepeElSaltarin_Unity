44d3a29bcab6fd09ed76300ab827b046d0abced7
SAbelle <sabelle@alumno.huergo.edu.ar>
2021-11-13 11:54:04 -0300

    Carpeta con recursos y movimiento basico del personaje agregados

--- PepeElSaltarin_Project/Assets/Scripts/SC_playercontroller.cs
+++ PepeElSaltarin_Project/Assets/Scripts/SC_playercontroller.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class SC_playercontroller : MonoBehaviour
 {
     private float movementSpeed = 5f;
+    private bool jumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movementh = Input.GetAxis("Horizontal");
-        float movementv = Input.GetAxis("Vertical");
+        Transform t = this.GetComponent<Transform>();
 
-        transform.position = transform.position + new Vector3(movementh * movementSpeed * Time.deltaTime, movementv * movementSpeed * Time.deltaTime, 0);
+        //Right movement
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            t.localScale = new Vector2(1,1); //Flips the sprite and his components
+            t.position = new Vector2(t.position.x + Time.deltaTime * movementSpeed, t.position.y);
+        }
+        //Left movement
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            t.localScale = new Vector2(1,-1); //Flips the sprite and his components
+            t.position = new Vector2(t.position.x - Time.deltaTime * movementSpeed, t.position.y);
+        }
+        //Jump action
+        if (Input.GetKey(KeyCode.Space))
+        {
+            t.position = new Vector2(t.position.x, t.position.y + Time.deltaTime * movementSpeed);
+            jumping = true;
+        }
     }
 }

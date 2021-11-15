5992603079af5f8a0ec9ec8c8f5554d73251f9d1
SAbelle <sabelle@alumno.huergo.edu.ar>
2021-11-15 12:26:49 -0300

    Falta movimiento de camara y fondo

---
+++ PepeElSaltarin_Project/Assets/Scripts/SC_player.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_player : MonoBehaviour
+{
+    private float movementSpeed = 250f;
+    private float movedirection;
+    private float gravity = 250f;
+
+    private bool jumping = false;
+    private bool isgrounded;
+    public bool attacking = false;
+    
+    public Transform pisoCheck;
+    public LayerMask terrainmask;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Attacking", attacking);
+
+        Transform t = this.GetComponent<Transform>();
+
+        movedirection = 0; //Default movement direction
+
+        //Right movement
+        if (Input.GetKey(KeyCode.D))
+        {
+            movedirection = 1;
+            t.localScale = new Vector2(1, 1); //Flips the sprite and his components
+        }
+        //Left movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            movedirection = -1;
+            t.localScale = new Vector2(1, -1); //Flips the sprite and his components
+        }
+        //Jump action
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jumping = true;
+        }
+        //Attack action starts
+        if (Input.GetKey(KeyCode.G))
+        {
+            attacking = true;
+        }
+        //Attack action ends
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            attacking = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+            collision.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        isgrounded = Physics2D.OverlapCircle(pisoCheck.position,0.02f,terrainmask);
+
+        Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+
+        if(jumping == true && isgrounded == true)
+        {
+            rb.AddForce(new Vector2(0,gravity));
+            jumping = false;
+        }
+        if(movedirection > 0)
+        {
+            rb.velocity = new Vector2(Time.deltaTime * movementSpeed,rb.velocity.y);
+        }
+        if (movedirection < 0)
+        {
+            rb.velocity = new Vector2(-Time.deltaTime * movementSpeed,rb.velocity.y);
+        }
+        if(movedirection == 0)
+        {
+            rb.velocity = new Vector2(0,rb.velocity.y);
+        }
+    }
+}
--- README.txt
+++ README.txt
@@ -9,4 +9,4 @@ UNITY VERSIÓN 2020.3.14
 
 JUEGO PEPE EL SALTARÍN (UNITY 2020.3.14)
 TP Proyecto 2
-Santiago Abelle, Gabriel Atienza y Ezequiel Coronel
+Santiago Abelle, Gabriel Atienza, Ezequiel Coronel y Juan Manuel Costarelli
